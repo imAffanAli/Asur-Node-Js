00730768d5ba9c86bbdde8f8cf1d5fc0b98d3ba5
imAffanAli <maffanali02@gmail.com>
2024-06-27 13:47:09 +0500

    Creating a PR request

--- dist/index.js
+++ dist/index.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is currently running at http://localhost:${port}`);
 });
